154f4ba72a3fca71c5ceba33d2cb17e463cdd833
yvovc <yehorvovchok@gmail.com>
2020-09-26 22:24:03 +0300

    Improve query building for streaming sessions selecting.

--- src/main/java/com/ubisoft/streaming/testtask/p2pmediator/dataservice/implementation/JooqStreamingSessionDataService.java
+++ src/main/java/com/ubisoft/streaming/testtask/p2pmediator/dataservice/implementation/JooqStreamingSessionDataService.java
@@ -1,11 +1,13 @@
 package com.ubisoft.streaming.testtask.p2pmediator.dataservice.implementation;
 
-import com.ubisoft.streaming.testtask.p2pmediator.dataservice.IStreamingSessionDataService;
 import com.ubisoft.streaming.testtask.p2pmediator.auth.Peer;
-import com.ubisoft.streaming.testtask.p2pmediator.dto.Game;
+import com.ubisoft.streaming.testtask.p2pmediator.dataservice.IStreamingSessionDataService;
+import com.ubisoft.streaming.testtask.p2pmediator.dto.VideoGame;
 import com.ubisoft.streaming.testtask.p2pmediator.dto.streaming.session.StreamingSession;
 import com.ubisoft.streaming.testtask.p2pmediator.dto.streaming.session.StreamingSessionStatus;
 import org.jooq.DSLContext;
+import org.jooq.SelectQuery;
+import org.springframework.util.CollectionUtils;
 
 import java.util.List;
 
@@ -21,25 +23,27 @@ public class JooqStreamingSessionDataService implements IStreamingSessionDataSer
 
     @Override
     public void createStreamingSession(final Peer peer,
-                                       final Game game) {
+                                       final VideoGame videoGame) {
         dslContext.insertInto(STREAMING_SESSION)
                 .set(STREAMING_SESSION.HOST_ID, peer.getId())
-                .set(STREAMING_SESSION.VIDEO_GAME_ID, game.getId())
+                .set(STREAMING_SESSION.VIDEO_GAME_ID, videoGame.getId())
                 .set(STREAMING_SESSION.STREAMING_SESSION_STATUS, StreamingSessionStatus.NEW)
                 .execute();
     }
 
     @Override
     public List<StreamingSession> getStreamingSessions(final List<StreamingSessionStatus> statuses) {
-        return dslContext.select(
+        final SelectQuery<?> selectQuery = dslContext.select(
                 STREAMING_SESSION.ID,
                 STREAMING_SESSION.HOST_ID,
                 STREAMING_SESSION.STREAMING_SESSION_STATUS,
                 VIDEO_GAME.NAME.as(StreamingSession.VIDEO_GAME_NAME_FIELD_NAME)
         )
                 .from(STREAMING_SESSION)
-                .join(VIDEO_GAME).on(STREAMING_SESSION.VIDEO_GAME_ID.eq(VIDEO_GAME.ID))
-                .where(STREAMING_SESSION.STREAMING_SESSION_STATUS.in(statuses))
-                .fetchInto(StreamingSession.class);
+                .join(VIDEO_GAME).on(STREAMING_SESSION.VIDEO_GAME_ID.eq(VIDEO_GAME.ID)).getQuery();
+        if (!CollectionUtils.isEmpty(statuses)) {
+            selectQuery.addConditions(STREAMING_SESSION.STREAMING_SESSION_STATUS.in(statuses));
+        }
+        return selectQuery.fetchInto(StreamingSession.class);
     }
 }
