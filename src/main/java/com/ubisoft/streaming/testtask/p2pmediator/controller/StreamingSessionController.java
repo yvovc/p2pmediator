f10c3a2758c76dc847c8a3a87ac72c3a12de6ff5
yvovc <yehorvovchok@gmail.com>
2020-09-26 16:30:32 +0300

    Add API to get streaming sessions list.

--- src/main/java/com/ubisoft/streaming/testtask/p2pmediator/controller/StreamingSessionController.java
+++ src/main/java/com/ubisoft/streaming/testtask/p2pmediator/controller/StreamingSessionController.java
@@ -2,12 +2,14 @@ package com.ubisoft.streaming.testtask.p2pmediator.controller;
 
 import com.ubisoft.streaming.testtask.p2pmediator.auth.Peer;
 import com.ubisoft.streaming.testtask.p2pmediator.dto.Game;
+import com.ubisoft.streaming.testtask.p2pmediator.dto.streaming.session.StreamingSession;
+import com.ubisoft.streaming.testtask.p2pmediator.dto.streaming.session.StreamingSessionStatus;
 import com.ubisoft.streaming.testtask.p2pmediator.service.StreamingSessionService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/streaming-session")
@@ -17,6 +19,12 @@ public class StreamingSessionController {
     @Autowired
     public StreamingSessionController(final StreamingSessionService streamingSessionService) {
         this.streamingSessionService = streamingSessionService;
+    }
+
+    @GetMapping
+    public List<StreamingSession> getStreamingSessions(@AuthenticationPrincipal final Peer peer,
+                                                       @RequestParam final StreamingSessionStatus status) {
+        return streamingSessionService.getStreamingSessions(peer, status);
     }
 
     @PostMapping
