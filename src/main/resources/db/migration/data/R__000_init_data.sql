539e677a010d1d8305e183ce4eb9ab80ff22988e
yvovc <yehorvovchok@gmail.com>
2020-09-26 22:30:06 +0300

    Add data initialization script.

---
+++ src/main/resources/db/migration/data/R__000_init_data.sql
@@ -0,0 +1,19 @@
+SET SEARCH_PATH TO p2pmediatordb;
+
+INSERT INTO streaming_session_status(value) VALUES
+    ('new'),
+    ('active'),
+    ('finished');
+
+INSERT INTO view_request_status(value) VALUES
+    ('new'),
+    ('ready'),
+    ('completed');
+
+INSERT INTO video_game(name, description) VALUES
+    ('Need For Speed MW', 'Some Arcade racing'),
+    ('CS:GO', 'Cyberdisciplined Shooter');
+
+INSERT INTO peer(id, username, password) VALUES
+    ('67df8472-d30e-4cb9-bce3-e7fad5f93827', 'super_streamer', '$2a$10$FlbJeRNIWllRrUjVNR5ttO4ICHNixiswRcFdJbt2HiLy2EK1u/nEO'),
+    ('49489489-f0b2-4254-87e1-4c8af47ee379', 'super_viewer','$2a$10$FlbJeRNIWllRrUjVNR5ttO4ICHNixiswRcFdJbt2HiLy2EK1u/nEO');
