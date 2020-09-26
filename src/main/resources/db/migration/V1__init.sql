d208a7599b7a2ba24c3321105d488fdea7d09b6e
yvovc <yehorvovchok@gmail.com>
2020-09-26 22:09:33 +0300

    Add 'NOT NULL' to some table declarations.

--- src/main/resources/db/migration/V1__init.sql
+++ src/main/resources/db/migration/V1__init.sql
@@ -26,26 +26,26 @@ CREATE TABLE p2pmediatordb.streaming_session (
     id SERIAL PRIMARY KEY,
     host_id UUID REFERENCES p2pmediatordb.peer(id) ON DELETE CASCADE,
     video_game_id INTEGER REFERENCES p2pmediatordb.video_game(id) ON DELETE CASCADE,
-    streaming_session_status INTEGER REFERENCES p2pmediatordb.streaming_session_status(id) ON DELETE CASCADE
+    streaming_session_status INTEGER REFERENCES p2pmediatordb.streaming_session_status(id) ON DELETE CASCADE NOT NULL
 );
 
 CREATE TABLE p2pmediatordb.view_request (
     id SERIAL PRIMARY KEY,
     viewer_id UUID REFERENCES p2pmediatordb.peer(id) ON DELETE CASCADE,
-    streaming_session_id INTEGER REFERENCES p2pmediatordb.streaming_session(id) ON DELETE CASCADE,
-    view_request_status INTEGER REFERENCES p2pmediatordb.view_request_status(id) ON DELETE CASCADE
+    streaming_session_id INTEGER REFERENCES p2pmediatordb.streaming_session(id) ON DELETE CASCADE NOT NULL,
+    view_request_status INTEGER REFERENCES p2pmediatordb.view_request_status(id) ON DELETE CASCADE NOT NULL
 );
 
 CREATE TABLE p2pmediatordb.streaming_session_endpoint (
     id SERIAL PRIMARY KEY,
-    streaming_session_id INTEGER REFERENCES p2pmediatordb.streaming_session(id) ON DELETE CASCADE,
+    streaming_session_id INTEGER REFERENCES p2pmediatordb.streaming_session(id) ON DELETE CASCADE NOT NULL,
     host VARCHAR,
     port INTEGER
 );
 
 CREATE TABLE p2pmediatordb.view_request_endpoint (
     id SERIAL PRIMARY KEY,
-    view_request_id INTEGER REFERENCES p2pmediatordb.view_request(id) ON DELETE CASCADE,
+    view_request_id INTEGER REFERENCES p2pmediatordb.view_request(id) ON DELETE CASCADE NOT NULL,
     host VARCHAR,
     port INTEGER
 );
