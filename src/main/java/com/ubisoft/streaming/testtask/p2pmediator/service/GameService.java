c16f585b423fff2a906f7008983d50bc42cb5386
yvovc <yehorvovchok@gmail.com>
2020-09-26 22:22:23 +0300

    Initialize game service and DTO.

---
+++ src/main/java/com/ubisoft/streaming/testtask/p2pmediator/service/GameService.java
@@ -0,0 +1,11 @@
+package com.ubisoft.streaming.testtask.p2pmediator.service;
+
+import com.ubisoft.streaming.testtask.p2pmediator.dto.VideoGame;
+import org.springframework.stereotype.Service;
+
+@Service
+public class GameService {
+    public boolean exists(final VideoGame videoGame) {
+        return false;
+    }
+}
