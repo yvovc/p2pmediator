af43216ebe9968d2f6e6903bb8af0693ffaf024a
yvovc <yehorvovchok@gmail.com>
2020-09-26 12:35:04 +0300

    Add DatabaseMigrationConfig.java to perform embedded Postgres migration on startup.

---
+++ src/main/java/com/ubisoft/streaming/testtask/p2pmediator/config/DatabaseMigrationConfig.java
@@ -0,0 +1,26 @@
+package com.ubisoft.streaming.testtask.p2pmediator.config;
+
+import org.flywaydb.core.Flyway;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+
+import javax.annotation.PostConstruct;
+import javax.sql.DataSource;
+
+@Configuration
+public class DatabaseMigrationConfig {
+    @Autowired
+    DataSource ds;
+
+    @PostConstruct
+    public void migrateWithFlyway() {
+        Flyway flyway = Flyway.configure()
+                .dataSource(ds)
+                .schemas("p2pmediatordb")
+                .defaultSchema("p2pmediatordb")
+                .locations("db/migration/")
+                .load();
+
+        flyway.migrate();
+    }
+}
