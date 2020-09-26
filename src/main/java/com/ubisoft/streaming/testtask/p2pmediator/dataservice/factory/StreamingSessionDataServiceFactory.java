0f046eae1943cfd582c724318c9588b481c7a04a
yvovc <yehorvovchok@gmail.com>
2020-09-26 15:15:03 +0300

    Add JooqStreamingSessionDataService implementation to factory.

--- src/main/java/com/ubisoft/streaming/testtask/p2pmediator/dataservice/factory/StreamingSessionDataServiceFactory.java
+++ src/main/java/com/ubisoft/streaming/testtask/p2pmediator/dataservice/factory/StreamingSessionDataServiceFactory.java
@@ -1,11 +1,13 @@
 package com.ubisoft.streaming.testtask.p2pmediator.dataservice.factory;
 
+import com.google.common.collect.ImmutableMap;
 import com.ubisoft.streaming.testtask.p2pmediator.dataservice.IStreamingSessionDataService;
 import com.ubisoft.streaming.testtask.p2pmediator.dataservice.implementation.JooqStreamingSessionDataService;
 import org.jooq.DSLContext;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import javax.annotation.PostConstruct;
 import java.util.Map;
 
 @Component
@@ -13,6 +15,12 @@ public class StreamingSessionDataServiceFactory {
     private static Map<String, IStreamingSessionDataService> STREAMING_SESSION_TYPE_DATA_SERVICE_MAP;
 
     private final DSLContext dslContext;
+
+    @PostConstruct
+    public void init() {
+        STREAMING_SESSION_TYPE_DATA_SERVICE_MAP = ImmutableMap.<String, IStreamingSessionDataService>builder()
+                .put("JooqDSL", new JooqStreamingSessionDataService(dslContext)).build();
+    }
 
     @Autowired
     public StreamingSessionDataServiceFactory(final DSLContext dslContext) {
