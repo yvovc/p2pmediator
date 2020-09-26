0f98dacb53cbe11cd957a926d2dafcf34e2d4f39
yvovc <yehorvovchok@gmail.com>
2020-09-26 15:12:23 +0300

    Preconfigure authentication.

--- src/main/java/com/ubisoft/streaming/testtask/p2pmediator/config/SecurityConfiguration.java
+++ src/main/java/com/ubisoft/streaming/testtask/p2pmediator/config/SecurityConfiguration.java
@@ -1,20 +1,42 @@
 package com.ubisoft.streaming.testtask.p2pmediator.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private UserDetailsService userDetailsService;
+
+    @Bean
+    public PasswordEncoder encoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationProvider admindbAuthenticationProvider() {
+        final DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+        provider.setUserDetailsService(userDetailsService);
+        provider.setPasswordEncoder(encoder());
+        return provider;
+    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.requestMatchers()
                 .antMatchers("/**")
                 .and()
-                .authorizeRequests().antMatchers("/test").permitAll();
-
+                .authorizeRequests().antMatchers("/streaming-session/**").hasRole("PEER");
     }
 }
