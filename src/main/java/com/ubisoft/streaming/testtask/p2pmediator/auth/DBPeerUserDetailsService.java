edf724fb3fbaa900f1a9ac74ab139c0a687fc011
yvovc <yehorvovchok@gmail.com>
2020-09-26 22:05:52 +0300

    Implement user details service logic.

--- src/main/java/com/ubisoft/streaming/testtask/p2pmediator/auth/DBPeerUserDetailsService.java
+++ src/main/java/com/ubisoft/streaming/testtask/p2pmediator/auth/DBPeerUserDetailsService.java
@@ -1,18 +1,37 @@
 package com.ubisoft.streaming.testtask.p2pmediator.auth;
 
+import com.ubisoft.streaming.testtask.p2pmediator.p2pmediatordb.tables.records.PeerRecord;
+import org.jooq.DSLContext;
+import org.jooq.tools.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.core.authority.AuthorityUtils;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
-import java.util.UUID;
+import static com.ubisoft.streaming.testtask.p2pmediator.p2pmediatordb.Tables.PEER;
 
 @Service
 public class DBPeerUserDetailsService implements UserDetailsService {
 
+    @Autowired
+    private DSLContext dslContext;
+
     @Override
     public UserDetails loadUserByUsername(final String username) throws UsernameNotFoundException {
-        return new Peer(UUID.randomUUID(), username, "password", AuthorityUtils.createAuthorityList("PEER"));
+        if (StringUtils.isBlank(username)) {
+            throw new AccessDeniedException("Username cannot be blank");
+        }
+        final PeerRecord peerRecord = dslContext.selectFrom(PEER)
+                .where(PEER.USERNAME.eq(username))
+                .fetchOneInto(PeerRecord.class);
+        if (peerRecord == null) {
+            throw new AccessDeniedException(String.format("User '%s' wasn't found", username));
+        }
+        return new Peer(peerRecord.getId(),
+                peerRecord.getUsername(), peerRecord.getPassword(),
+                AuthorityUtils.createAuthorityList("ROLE_PEER"));
     }
 }
