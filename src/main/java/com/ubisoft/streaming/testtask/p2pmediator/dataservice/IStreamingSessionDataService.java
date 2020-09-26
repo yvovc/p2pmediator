a9441cd8a32acd52adc46b7721fe3189c4256e01
yvovc <yehorvovchok@gmail.com>
2020-09-26 22:23:15 +0300

    Apply adjustments after Game.java -> VideoGame.java renaming.

--- src/main/java/com/ubisoft/streaming/testtask/p2pmediator/dataservice/IStreamingSessionDataService.java
+++ src/main/java/com/ubisoft/streaming/testtask/p2pmediator/dataservice/IStreamingSessionDataService.java
@@ -1,7 +1,7 @@
 package com.ubisoft.streaming.testtask.p2pmediator.dataservice;
 
 import com.ubisoft.streaming.testtask.p2pmediator.auth.Peer;
-import com.ubisoft.streaming.testtask.p2pmediator.dto.Game;
+import com.ubisoft.streaming.testtask.p2pmediator.dto.VideoGame;
 import com.ubisoft.streaming.testtask.p2pmediator.dto.streaming.session.StreamingSession;
 import com.ubisoft.streaming.testtask.p2pmediator.dto.streaming.session.StreamingSessionStatus;
 
@@ -9,7 +9,7 @@ import java.util.List;
 
 public interface IStreamingSessionDataService {
     void createStreamingSession(final Peer peer,
-                                final Game gameToStream);
+                                final VideoGame videoGameToStream);
 
     List<StreamingSession> getStreamingSessions(final List<StreamingSessionStatus> statuses);
 }
