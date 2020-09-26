97219953e7836b35dc1c413617608cdba58355b0
yvovc <yehorvovchok@gmail.com>
2020-09-26 16:29:41 +0300

    Add method to IStreamingSessionDataService.java to fetch streaming sessions.

--- src/main/java/com/ubisoft/streaming/testtask/p2pmediator/dataservice/IStreamingSessionDataService.java
+++ src/main/java/com/ubisoft/streaming/testtask/p2pmediator/dataservice/IStreamingSessionDataService.java
@@ -2,8 +2,14 @@ package com.ubisoft.streaming.testtask.p2pmediator.dataservice;
 
 import com.ubisoft.streaming.testtask.p2pmediator.auth.Peer;
 import com.ubisoft.streaming.testtask.p2pmediator.dto.Game;
+import com.ubisoft.streaming.testtask.p2pmediator.dto.streaming.session.StreamingSession;
+import com.ubisoft.streaming.testtask.p2pmediator.dto.streaming.session.StreamingSessionStatus;
+
+import java.util.List;
 
 public interface IStreamingSessionDataService {
     void createStreamingSession(final Peer peer,
                                 final Game gameToStream);
+
+    List<StreamingSession> getStreamingSessions(final List<StreamingSessionStatus> statuses);
 }
