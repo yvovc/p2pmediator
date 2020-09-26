88203ea1784d2192d42d9b32322fb6115fe47d94
yvovc <yehorvovchok@gmail.com>
2020-09-26 22:10:43 +0300

    Adjust security config with CSRF checks disable and auth provider.

--- src/main/java/com/ubisoft/streaming/testtask/p2pmediator/config/SecurityConfiguration.java
+++ src/main/java/com/ubisoft/streaming/testtask/p2pmediator/config/SecurityConfiguration.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -32,11 +33,21 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
         return provider;
     }
 
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) {
+        auth.authenticationProvider(admindbAuthenticationProvider());
+    }
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.requestMatchers()
+        http
+                .requestMatchers()
                 .antMatchers("/**")
                 .and()
-                .authorizeRequests().antMatchers("/streaming-session/**").hasRole("PEER");
+                .httpBasic()
+                .and()
+                .authorizeRequests().antMatchers("/streaming-session/**").hasRole("PEER")
+                .and()
+                .csrf().disable();
     }
 }
