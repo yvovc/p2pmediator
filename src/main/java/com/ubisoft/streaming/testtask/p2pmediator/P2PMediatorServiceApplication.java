9d4ede75bafebd7c33026a3f6fde6b20a632646e
yvovc <yehorvovchok@gmail.com>
2020-09-26 11:50:19 +0300

    Initialize Spring Boot Gradle project with required dependencies.

---
+++ src/main/java/com/ubisoft/streaming/testtask/p2pmediator/P2PMediatorServiceApplication.java
@@ -0,0 +1,12 @@
+package com.ubisoft.streaming.testtask.p2pmediator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class P2PMediatorServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(P2PMediatorServiceApplication.class, args);
+	}
+}
