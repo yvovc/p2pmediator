0edf5cfa953c2a3777c22d6cf2ba725e36cd60ee
yvovc <yehorvovchok@gmail.com>
2020-09-27 16:12:14 +0300

    Extend IStreamingSessionDataService.java DAL interface.

--- src/main/java/com/ubisoft/streaming/testtask/p2pmediator/dataservice/IStreamingSessionDataService.java
+++ src/main/java/com/ubisoft/streaming/testtask/p2pmediator/dataservice/IStreamingSessionDataService.java
@@ -3,13 +3,32 @@ package com.ubisoft.streaming.testtask.p2pmediator.dataservice;
 import com.ubisoft.streaming.testtask.p2pmediator.auth.Peer;
 import com.ubisoft.streaming.testtask.p2pmediator.dto.VideoGame;
 import com.ubisoft.streaming.testtask.p2pmediator.dto.streaming.session.StreamingSession;
+import com.ubisoft.streaming.testtask.p2pmediator.dto.streaming.session.StreamingSessionEndpoint;
+import com.ubisoft.streaming.testtask.p2pmediator.dto.streaming.session.StreamingSessionRole;
 import com.ubisoft.streaming.testtask.p2pmediator.dto.streaming.session.StreamingSessionStatus;
 
 import java.util.List;
+import java.util.UUID;
 
 public interface IStreamingSessionDataService {
-    void createStreamingSession(final Peer peer,
-                                final VideoGame videoGameToStream);
+    StreamingSession createStreamingSession(final Peer peer,
+                                            final VideoGame videoGameToStream);
 
     List<StreamingSession> getStreamingSessions(final List<StreamingSessionStatus> statuses);
+
+    StreamingSessionRole getPeerRole(final UUID peerId,
+                                     final Integer streamingSessionId);
+
+    boolean exists(final Integer streamingSessionId);
+
+    StreamingSession getStreamingSession(final Integer streamingSessionId);
+
+    StreamingSessionStatus updateStreamingSessionStatus(final Integer streamingSessionId,
+                                                        final StreamingSessionStatus newStatus);
+
+    List<StreamingSessionEndpoint> getStreamingSessionEndpoints(final Integer streamingSessionId,
+                                                                final List<StreamingSessionStatus> statuses);
+
+    List<StreamingSessionEndpoint> addStreamingSessionEndpoints(final Integer streamingSessionId,
+                                                                final List<StreamingSessionEndpoint> streamSessionEndpoints);
 }
