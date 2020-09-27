ebaa078ae45bf8e0716e5d0cd3384d62f9175fc9
yvovc <yehorvovchok@gmail.com>
2020-09-28 00:18:49 +0300

    Init GameService.java mock service.

--- src/main/java/com/ubisoft/streaming/testtask/p2pmediator/service/GameService.java
+++ src/main/java/com/ubisoft/streaming/testtask/p2pmediator/service/GameService.java
@@ -6,6 +6,6 @@ import org.springframework.stereotype.Service;
 @Service
 public class GameService {
     public boolean exists(final VideoGame videoGame) {
-        return false;
+        return true;
     }
 }
