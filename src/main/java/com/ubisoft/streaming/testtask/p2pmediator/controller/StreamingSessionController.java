8a640a62d5d8629e9745d09d5130e2b0231a9957
yvovc <yehorvovchok@gmail.com>
2020-09-27 16:10:32 +0300

    Add endpoints to StreamingSessionController.java.

--- src/main/java/com/ubisoft/streaming/testtask/p2pmediator/controller/StreamingSessionController.java
+++ src/main/java/com/ubisoft/streaming/testtask/p2pmediator/controller/StreamingSessionController.java
@@ -3,6 +3,7 @@ package com.ubisoft.streaming.testtask.p2pmediator.controller;
 import com.ubisoft.streaming.testtask.p2pmediator.auth.Peer;
 import com.ubisoft.streaming.testtask.p2pmediator.dto.VideoGame;
 import com.ubisoft.streaming.testtask.p2pmediator.dto.streaming.session.StreamingSession;
+import com.ubisoft.streaming.testtask.p2pmediator.dto.streaming.session.StreamingSessionEndpoint;
 import com.ubisoft.streaming.testtask.p2pmediator.dto.streaming.session.StreamingSessionStatus;
 import com.ubisoft.streaming.testtask.p2pmediator.service.StreamingSessionService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -28,8 +29,29 @@ public class StreamingSessionController {
     }
 
     @PostMapping
-    public void createStreamingSession(@AuthenticationPrincipal final Peer peer,
-                                       @RequestBody final VideoGame videoGameToStream) {
-        streamingSessionService.createStreamingSession(peer, videoGameToStream);
+    public StreamingSession createStreamingSession(@AuthenticationPrincipal final Peer peer,
+                                                   @RequestBody final VideoGame videoGameToStream) {
+        return streamingSessionService.createStreamingSession(peer, videoGameToStream);
+    }
+
+    @PutMapping("/{streamingSessionId}/status")
+    public StreamingSessionStatus updateStreamingSessionStatus(@AuthenticationPrincipal final Peer peer,
+                                                               @PathVariable final Integer streamingSessionId,
+                                                               @RequestBody final StreamingSessionStatus newStatus) {
+        return streamingSessionService.updateStreamingSessionStatus(peer, streamingSessionId, newStatus);
+    }
+
+    @GetMapping("/{streamingSessionId}/endpoint")
+    public List<StreamingSessionEndpoint> getStreamingSessionEndpoints(@AuthenticationPrincipal final Peer peer,
+                                                                       @PathVariable final Integer streamingSessionId,
+                                                                       @RequestParam final List<StreamingSessionStatus> statuses) {
+        return streamingSessionService.getStreamingSessionEndpoints(peer, streamingSessionId, statuses);
+    }
+
+    @PostMapping("/{streamingSessionId}/endpoint")
+    public List<StreamingSessionEndpoint> addStreamSessionEndpoints(@AuthenticationPrincipal Peer peer,
+                                                                    @PathVariable final Integer streamingSessionId,
+                                                                    @RequestBody List<StreamingSessionEndpoint> streamSessionEndpoints) {
+        return streamingSessionService.addStreamingSessionEndpoints(peer, streamingSessionId, streamSessionEndpoints);
     }
 }
