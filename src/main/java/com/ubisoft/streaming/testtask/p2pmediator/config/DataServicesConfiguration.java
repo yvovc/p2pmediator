8cfea8f06132313c607beabf041076873ea3ff20
yvovc <yehorvovchok@gmail.com>
2020-09-28 00:23:31 +0300

    Add view request data service init in DataServicesConfiguration.java.

--- src/main/java/com/ubisoft/streaming/testtask/p2pmediator/config/DataServicesConfiguration.java
+++ src/main/java/com/ubisoft/streaming/testtask/p2pmediator/config/DataServicesConfiguration.java
@@ -1,7 +1,9 @@
 package com.ubisoft.streaming.testtask.p2pmediator.config;
 
 import com.ubisoft.streaming.testtask.p2pmediator.dataservice.IStreamingSessionDataService;
+import com.ubisoft.streaming.testtask.p2pmediator.dataservice.IViewRequestDataService;
 import com.ubisoft.streaming.testtask.p2pmediator.dataservice.factory.StreamingSessionDataServiceFactory;
+import com.ubisoft.streaming.testtask.p2pmediator.dataservice.factory.ViewRequestDataServiceFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
@@ -12,15 +14,25 @@ public class DataServicesConfiguration {
     @Value("${services.streaming-session.data-service.type}")
     private String streamingSessionDataServiceType;
 
+    @Value("${services.view-request.data-service.type}")
+    private String viewRequestDataServiceType;
+
     private final StreamingSessionDataServiceFactory streamingSessionDataServiceFactory;
+    private final ViewRequestDataServiceFactory viewRequestDataServiceFactory;
 
     @Autowired
-    public DataServicesConfiguration(final StreamingSessionDataServiceFactory streamingSessionDataServiceFactory) {
+    public DataServicesConfiguration(final StreamingSessionDataServiceFactory streamingSessionDataServiceFactory, ViewRequestDataServiceFactory viewRequestDataServiceFactory) {
         this.streamingSessionDataServiceFactory = streamingSessionDataServiceFactory;
+        this.viewRequestDataServiceFactory = viewRequestDataServiceFactory;
     }
 
     @Bean
     public IStreamingSessionDataService streamingSessionDataService() {
         return streamingSessionDataServiceFactory.getStreamingSessionDataService(streamingSessionDataServiceType);
+    }
+
+    @Bean
+    public IViewRequestDataService viewRequestDataService() {
+        return viewRequestDataServiceFactory.getViewRequestDataService(viewRequestDataServiceType);
     }
 }
